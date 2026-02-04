13b43d9a1954194c529fba8f912277ecf1311b33
PranavShadow <ompranav2003@gmail.com>
2026-02-05 03:37:51 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import bcrypt from "bcryptjs";
+import express from "express";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if(!fullName || !email || !password){
+      return res
+        .status(400)
+        .json({ message: "Missing details" });
+    }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be above 8 characters" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists." });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Inavlid User Data" });
+    }
+  } catch (e) {
+    console.log("Error in signup controller", e.message);
+    res.status(500).json({message: "Internal Server Error"})
+  }
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+
+export const logout = (req, res) => {
+  res.send("logout route");
+};
